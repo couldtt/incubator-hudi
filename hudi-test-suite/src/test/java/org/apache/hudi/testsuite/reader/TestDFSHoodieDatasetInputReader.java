37f91cb1d7f4c50f6f600bcbc517e7c7b879e168
Nishith Agarwal <nagarwal@uber.com>
2020-04-13 23:51:06 -0700

    more fixes

--- hudi-test-suite/src/test/java/org/apache/hudi/testsuite/reader/TestDFSHoodieDatasetInputReader.java
+++ hudi-test-suite/src/test/java/org/apache/hudi/testsuite/reader/TestDFSHoodieDatasetInputReader.java
@@ -28,6 +28,7 @@ import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.utilities.UtilitiesTestBase;
 import org.apache.hudi.utilities.schema.FilebasedSchemaProvider;
 
+
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.spark.api.java.JavaRDD;
@@ -36,7 +37,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -53,15 +53,15 @@ public class TestDFSHoodieDatasetInputReader extends UtilitiesTestBase {
   }
 
   @AfterClass
-  public static void cleanupClass() throws Exception {
+  public static void cleanupClass() {
     UtilitiesTestBase.cleanupClass();
   }
 
   @Before
   public void setup() throws Exception {
     super.setup();
-    schemaProvider = new FilebasedSchemaProvider(Helpers.setupSchemaOnDFS("hudi-test-suite-config/complex-source.avsc"),
-        jsc);
+    schemaProvider = new FilebasedSchemaProvider(
+        Helpers.setupSchemaOnDFS("hudi-test-suite-config", "complex-source.avsc"), jsc);
     HoodieTestUtils.init(jsc.hadoopConfiguration(), dfsBasePath);
   }
 
